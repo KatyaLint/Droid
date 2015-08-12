7593a6f03af0cf170dc696ea2c8cb9e2f975eab9
#{arisprung} <#{arisprung@gmail.com}>
2015-08-12 12:09:46 +0300

    Installed Crashlytics

--- app/src/main/java/hellogbye/com/hellogbyeandroid/MainActivity.java
+++ app/src/main/java/hellogbye/com/hellogbyeandroid/MainActivity.java
@@ -4,13 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+
+      int i = 0;
+
+        int c = 2/i;
     }
 
     @Override
