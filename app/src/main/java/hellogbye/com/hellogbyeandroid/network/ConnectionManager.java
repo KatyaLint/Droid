c10339b58e0423f76ac95c2d4cc6844562396849
arisprung <#{arisprung@gmail.com}>
2016-08-01 14:50:09 +0300

    Added Progress Dialog in between 2nd and 3rd stage

--- app/src/main/java/hellogbye/com/hellogbyeandroid/network/ConnectionManager.java
+++ app/src/main/java/hellogbye/com/hellogbyeandroid/network/ConnectionManager.java
@@ -879,7 +879,7 @@ public class ConnectionManager {
             public void onErrorResponse(VolleyError error) {
                 listener.onError(Parser.parseErrorMessage(error));
             }
-        },false);
+        });
 
     }
 
