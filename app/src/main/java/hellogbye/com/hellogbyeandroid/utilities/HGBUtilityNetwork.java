b3bd671aea3ca8c66c377dcb2653307d9e25d378
kate <nyawka@kates-MacBook-Pro.local>
2016-12-15 17:54:23 +0200

    HEL-1831 - at every entrance to the app the enabled location pops out

--- app/src/main/java/hellogbye/com/hellogbyeandroid/utilities/HGBUtilityNetwork.java
+++ app/src/main/java/hellogbye/com/hellogbyeandroid/utilities/HGBUtilityNetwork.java
@@ -30,7 +30,7 @@ public class HGBUtilityNetwork {
     private static LocationManager lm;
 
     public static boolean haveNetworkConnection(Context context) {
-        
+
         boolean haveConnectedWifi = false;
         boolean haveConnectedMobile = false;
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
